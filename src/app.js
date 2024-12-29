ffece7cc5326e77f21c103c2880a8df286023ffa
Hyrul <xavier.leonard@outlook.com>
2024-12-29 06:55:58 +0100

    added signup/login features

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const emblemRoutes = require('./routes/emblemRoutes')
+const userRoutes = require('./routes/user')
 
 const app = express()
 
@@ -25,5 +26,6 @@ app.use((req, res, next) => {
 
 // ROUTES
 app.use('/api/emblems', emblemRoutes)
+app.use('/api/auth', userRoutes)
 
 module.exports = app
---
+++ src/controllers/user.js
@@ -0,0 +1,59 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+exports.signup = async (req, res) => {
+  const passwordRegex = /^(?=.*[A-Z])(?=.*\d).{6,}$/
+
+  if (!passwordRegex.test(req.body.password)) {
+    return res.status(400).json({ message: 'Password must contain at least one capitalized character, one number and six characters.' })
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+    // Creating the user object
+    const user = new User({
+      nickname: req.body.nickname,
+      email: req.body.email,
+      password: hashedPassword
+    })
+
+    await user.save();
+    res.status(201).json({ message: 'User created!' });
+    } catch (error) { 
+      res.status(500).json({ error: error.message || 'Error creating user.' })
+    }
+  }
+    
+
+
+
+// LOGIN
+exports.login = async (req, res) => {
+  try {
+    // Find user through email check
+    const user = await User.findOne({ email: req.body.email })
+    if (!user) {
+      return res.status(401).json({ message: 'User not found!' })
+    }    
+
+    // Checking the password
+    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    if (!validPassword) {
+      return res.status(401).json({ message: 'Incorrect email and/or password!' });
+    }
+
+    // All good? Sending the token
+    res.status(200).json({
+      userId: user._id,
+      token: jwt.sign(
+        { userId: user._id },
+        process.env.JWT_SECRET,
+        { expiresIn: '72h' }
+      )
+    })
+  } catch (error) {
+    res.status(500).json({ error: error.message || 'Login failed.' });
+  }
+}
