4df3489e1be734ded6858fc91e73ae21fff3f5b3
Hyrul <xavier.leonard@outlook.com>
2024-12-29 20:01:44 +0100

    added an auth middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = (req, res, next) => {
+  try {
+
+    // Extract the token from the Authorization header
+    const token = req.headers.authorization.split(' ')[1]
+    const decoded = jwt.verify(token, process.env.JWT_KEY)
+
+    // Add the decoded user ID to the request object for future use
+    req.auth = { userId: decoded.userId }
+    next()
+  } catch (error) {
+    return res.status(401).json({ message: 'Auth failed' })
+  }
+}
