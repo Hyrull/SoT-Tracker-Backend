c55548f3b33c14024ec406ad2cc67a789bc3a8e5
Hyrul <xavier.leonard@outlook.com>
2024-12-29 23:17:38 +0100

    changed default port

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const http = require('http')
 const app = require('./src/app')
 
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 10000)
 const server = http.createServer(app)
 
 
-server.listen(process.env.PORT || 3000)
+server.listen(process.env.PORT || 10000)
 console.log('Server is up!')
