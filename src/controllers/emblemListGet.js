91483a22d4103ac5d0846d959193517e13775164
Hyrul <xavier.leonard@outlook.com>
2024-12-29 22:39:37 +0100

    added a route to fetch comms

---
+++ src/controllers/emblemListGet.js
@@ -0,0 +1,20 @@
+const UserData = require('../models/userData')
+
+const emblemListGet = async (req, res) => {
+  const userId = req.auth.userId
+  try {
+    const user = await UserData.findOne({ userId })
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' })
+    }
+
+    console.log('User found:', user)
+
+    return res.status(200).json(user.sotData)
+  
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching your commendations' })
+  }
+}
+
+module.exports = { emblemListGet }
--- src/routes/emblemRoutes.js
+++ src/routes/emblemRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const emblemUpdateCtrl = require('../controllers/emblemUpdate')
+const emblemListCtrl = require('../controllers/emblemListGet')
 const auth = require('../middlewares/auth')
 
+router.get('/fetch', auth, emblemListCtrl.emblemListGet)
 router.patch('/update', auth, emblemUpdateCtrl.emblemUpdate)
 
 module.exports = router
